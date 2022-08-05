830ea9c625970d1ea08e763ed3c94ce5f663f4a1
Akira Akahori <akira.akahori@gmail.com>
2022-08-05 15:47:36 +0900

    Mono HomeControllerのhelloメソッドを修正する

--- tasklist/src/main/java/jp/gihyo/projava/tasklist/HomeController.java
+++ tasklist/src/main/java/jp/gihyo/projava/tasklist/HomeController.java
@@ -3,25 +3,18 @@ package jp.gihyo.projava.tasklist;
 import java.time.LocalDateTime;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
 
 import reactor.core.publisher.Mono;
 
 @Controller
 public class HomeController {
     @RequestMapping(value="/hello")
-    @ResponseBody
-    Mono<String> hello() {
-        return Mono.fromCallable(() -> """
-                <html>
-                    <head><title>Hello</title><head>
-                    <body>
-                        <h1>Hello</h1>
-                        It works!<br>
-                        現在時刻は%sです。
-                    </body>
-                <html>
-                """.formatted(LocalDateTime.now()));
+    Mono<String> hello(Model model) {
+        return Mono.fromCallable(() -> {
+            model.addAttribute("time", LocalDateTime.now());
+            return "hello";
+        });
     }
 }
