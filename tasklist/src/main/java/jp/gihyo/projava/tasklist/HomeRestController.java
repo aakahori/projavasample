7fa4a6d1aef9f73b84af72d1371f1259be2dc90a
Akira Akahori <akira.akahori@gmail.com>
2022-08-05 14:56:59 +0900

    Mono タスクを追加するエンドポイントの作成

--- tasklist/src/main/java/jp/gihyo/projava/tasklist/HomeRestController.java
+++ tasklist/src/main/java/jp/gihyo/projava/tasklist/HomeRestController.java
@@ -3,9 +3,12 @@ package jp.gihyo.projava.tasklist;
 import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.UUID;
 
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import reactor.core.publisher.Mono;
 
@@ -23,4 +26,17 @@ public class HomeRestController {
                 現在時刻は%sです。
                 """.formatted(LocalDateTime.now()));
     }
+
+    @GetMapping("/restadd")
+    Mono<String> addItem(@RequestParam("task") String task,
+                    @RequestParam("deadline") String deadline) {
+        return Mono.fromCallable(() -> {
+            String id = UUID.randomUUID().toString().substring(0,8);
+            TaskItem item = new TaskItem(id, task, deadline, false);
+            taskItems.add(item);
+
+            return "タスクを追加しました。";
+        });
+    }
+    
 }
