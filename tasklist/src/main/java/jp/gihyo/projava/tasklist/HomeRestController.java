0246d707bf1996d7df9e716eb5890172b1528065
Akira Akahori <akira.akahori@gmail.com>
2022-08-05 14:19:06 +0900

    タスクの情報を保持するためのモデルの作成

--- tasklist/src/main/java/jp/gihyo/projava/tasklist/HomeRestController.java
+++ tasklist/src/main/java/jp/gihyo/projava/tasklist/HomeRestController.java
@@ -1,6 +1,8 @@
 package jp.gihyo.projava.tasklist;
 
 import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
 
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -9,6 +11,10 @@ import reactor.core.publisher.Mono;
 
 @RestController
 public class HomeRestController {
+
+    record TaskItem(String id, String task, String deadline, boolean done) {}
+    private List<TaskItem> taskItems = new ArrayList<>();
+
     @RequestMapping(value="/resthello")
     Mono<String> hello() {
         return Mono.fromCallable(() -> """
