a8071ba8e6d90669827140802a934e0d4a2bc71f
Akira Akahori <akira.akahori@gmail.com>
2022-08-06 13:29:45 +0900

    Mono TaskListDaoクラスにタスク情報を削除するメソッドを追加

--- tasklist/src/main/java/jp/gihyo/projava/tasklist/TaskListDao.java
+++ tasklist/src/main/java/jp/gihyo/projava/tasklist/TaskListDao.java
@@ -2,11 +2,15 @@ package jp.gihyo.projava.tasklist;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.r2dbc.core.R2dbcEntityTemplate;
+import org.springframework.data.relational.core.query.Query;
 import org.springframework.stereotype.Service;
 
 import jp.gihyo.projava.tasklist.HomeController.TaskItem;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
+
+import static org.springframework.data.relational.core.query.Query.query;
+import static org.springframework.data.relational.core.query.Criteria.where;
 
 @Service
 public class TaskListDao {
@@ -26,6 +30,13 @@ public class TaskListDao {
     public Flux<TaskItem> findAll() {
         return r2dbcTemplate.select(TaskItem.class)
                 .from("tasklist")
+                .all();
+    }
+
+    public Mono<Integer> delete(String id) {
+        return r2dbcTemplate.delete(TaskItem.class)
+                .from("tasklist")
+                .matching(query(where("id").is(id)))
                 .all();
     }
 }
